77a524529ed51e4e3f8500f1950b35702fe3f8a0
onesword0618 <ao.akua.leo@gmail.com>
2021-07-27 01:18:44 +0900

    Add: フッターでリンク予定のページを追加した

---
+++ src/pages/about.tsx
@@ -0,0 +1,25 @@
+/**
+ * Blog About Explain.
+ *
+ * Copyright (c) 2021.
+ * Kenichi Inoue.
+ */
+import * as React from 'react';
+import { Layout } from '../components/layout';
+
+/**
+ * Blog About Explain.
+ *
+ * @returns {React.ReactElement} components
+ */
+export default function About(): React.ReactElement {
+  return (
+    <div className="application">
+      <Layout>
+        <main>
+          <h2>About</h2>
+        </main>
+      </Layout>
+    </div>
+  );
+}
---
+++ src/pages/design.tsx
@@ -0,0 +1,25 @@
+/**
+ * Design Explain.
+ *
+ * Copyright (c) 2021.
+ * Kenichi Inoue.
+ */
+import * as React from 'react';
+import { Layout } from '../components/layout';
+
+/**
+ * Design Explain.
+ *
+ * @returns {React.ReactElement} components
+ */
+export default function Design(): React.ReactElement {
+  return (
+    <div className="application">
+      <Layout>
+        <main>
+          <h2>Design</h2>
+        </main>
+      </Layout>
+    </div>
+  );
+}
