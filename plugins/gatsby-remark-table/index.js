6328f28cca17a965dbfce58c27a38787c2745cbf
onesword0618 <ao.akua.leo@gmail.com>
2021-11-07 23:52:41 +0900

    Fix: tableが出力されていないところを修正した

--- plugins/gatsby-remark-table/index.js
+++ plugins/gatsby-remark-table/index.js
@@ -8,7 +8,7 @@ module.exports = ({ markdownAST }) => {
     // https://github.com/remarkjs/remark-rehype#use
     const html = toHtml(unified().use(remarkRehype).runSync(node));
     node.type = `html`;
-    node.data = `
+    node.value = `
       <div class="table-wrapper">
         ${html}
       </div>
