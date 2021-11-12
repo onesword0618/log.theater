2afd2114b3d4c952f4f66af1a3f3f8a85902f728
onesword0618 <ao.akua.leo@gmail.com>
2021-11-12 23:03:19 +0900

    Update: configを更新した

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,60 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-image`,
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map((edge) => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.updated,
+                  url: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
+                  guid: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
+                  custom_elements: [{ 'content:encoded': edge.node.html }],
+                });
+              });
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: {order: DESC, fields: frontmatter___created}
+                ) {
+                    edges {
+                      node {
+                        id
+                        frontmatter {
+                          title
+                          path
+                          updated
+                        }
+                        excerpt(format: PLAIN, truncate: true)
+                        html
+                      }
+                    }
+                  }
+              }
+            `,
+            output: "/rss.xml",
+            title: "log.theater feed",
+          },
+        ],
+      },
+    },
     `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-canonical-urls`,
