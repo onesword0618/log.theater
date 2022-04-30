66166f25eedabab6012a934ea43c7c6590c04e88
onesword0618 <ao.akua.leo@gmail.com>
2022-05-01 00:55:23 +0900

    Update: code-block
    コードブロックの表示がずれているので修正を実施した

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,7 @@ require('ress');
 require('github-markdown-css');
 require('prismjs/themes/prism.css');
 require('prismjs/plugins/line-numbers/prism-line-numbers.css');
+require('./src/css/code-block.css');
 require('./src/css/article.css');
 require('./src/css/article.css');
 require('./src/css/entryTemplate.css');
