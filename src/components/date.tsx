01bcf67c1aff012717463793df8aa395b3de30b6
onesword0618 <ao.akua.leo@gmail.com>
2023-06-04 17:38:44 +0900

    Update: date #122

--- src/components/date.tsx
+++ src/components/date.tsx
@@ -1,30 +1,32 @@
 /**
- * Entry Templete Component Parts.
- *
- * Copyright (c) 2021.
- * Kenichi Inoue.
+ * @file The time element.
+ * @see https://html.spec.whatwg.org/multipage/text-level-semantics.html#the-time-element
+ * @see https://html.spec.whatwg.org/multipage/text-level-semantics.html#the-i-element
+ * @copyright @author Kenichi Inoue <ao.akua.leo@gmail.com> 2021.
  */
-import * as React from 'react';
+import { ComponentType } from 'react';
+import { Icon } from './icon';
 
 type Props = {
   className: string;
   caption: string;
-  date?: string | undefined;
+  date?: string | null;
 };
 
 /**
- * Date Component Parts.
- * @param {Props} props date
- * @returns {React.ReactElement} components
+ * Date Component Part.
+ * @param {{ className: string, caption: string, date?: string }} props time property
+ * @returns {ComponentType} component
  */
-export const Date: React.FC<Props> = (props: Props): React.ReactElement => {
+export const Date: ComponentType<Props> = (props) => {
   const { caption, className, date } = props;
   return (
     <time
       dateTime={className}
       style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}
     >
-      {caption} <i className="clock">{date}</i>
+      <Icon name={`clock`} size={`1em`} /> {caption}
+      <i className="clock">{date}</i>
     </time>
   );
 };
